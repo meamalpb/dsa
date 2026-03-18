b8ce3f82f760ce17649ea4b3f8a3bfb2b403f78a
Amal PB <321amalpb.com@gmail.com>
2026-03-18 11:28:05 +0530

    two sum ii

---
+++ arrays_and_hashing/medium/two_integer_sum.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+# arrays/medium/two_integer_sum.rb
+
+# Problem
+# https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
+# @param {Integer[]} numbers
+# @param {Integer} target
+# @return {Integer[]}
+def two_sum(nums, target)
+  l = 0
+  r = nums.length - 1
+  while l < r
+    sum = nums[l] + nums[r]
+    if sum == target
+      return [l + 1, r + 1]
+    elsif sum < target
+      l += 1
+    else
+      r -= 1
+    end
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  p two_sum([2, 7, 11, 15], 9)
+  p two_sum([2, 3, 4], 6)
+  p two_sum([-1, 0], -1)
+end
