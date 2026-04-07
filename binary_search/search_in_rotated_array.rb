fd399c4e1b5512ba55099dd68e6b04c757abef8d
Amal PB <321amalpb.com@gmail.com>
2026-04-07 15:54:24 +0530

    search in rotated array

---
+++ binary_search/search_in_rotated_array.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+# binary_search/easy/binary_search.rb
+
+# Problem
+# http://leetcode.com/problems/binary-search/description/
+
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer}
+def search(nums, target)
+  l = 0
+  r = nums.length - 1
+  while l <= r
+    mid = (l + r) / 2
+    if nums[mid] == target
+      return mid
+    elsif nums[mid] > nums[r]
+      if target > nums[mid]
+        l = mid + 1
+      elsif nums[l] <= target
+        r = mid - 1
+      else
+        l = mid + 1
+      end
+    elsif nums[r] >= target
+      if nums[mid] > target
+        r = mid - 1
+      else
+        l = mid + 1
+      end
+    else
+      r = mid - 1
+    end
+  end
+  -1
+end
+
+if __FILE__ == $PROGRAM_NAME
+  p search([4, 5, 6, 7, 0, 1, 2], 0)
+  p search([4, 5, 6, 7, 0, 1, 2], 3)
+  p search([1], 0)
+  p search([5, 1, 3], 5)
+  p search([1, 3], 3)
+
+end
