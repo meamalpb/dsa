f1cb02f9bd623b75721fa10c38f63948cbf691d3
Amal PB <321amalpb.com@gmail.com>
2026-07-18 19:25:11 +0530

    reverse string

---
+++ two_pointers/easy/reverse_string.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+# two_pointers/easy/reverse_string.rb
+
+# Problem
+# https://leetcode.com/problems/reverse-string/
+
+# @param {Character[]} s
+# @return {Void} Do not return anything, modify s in-place instead.
+def reverse_string(s)
+    i=0
+    n=s.length
+    while i<(n/2)
+        temp = s[n-i-1]
+        s[n-i-1]=s[i]
+        s[i]=temp
+        i+=1
+    end
+    s
+end
+
+
+if __FILE__ == $PROGRAM_NAME
+    p reverse_string(["h","e","l","l","o"])
+    p reverse_string(["H","a","n","n","a","h"]) 
+end
