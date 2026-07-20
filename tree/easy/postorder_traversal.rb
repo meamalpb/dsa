6184d9411188b8f81b3a843ed5c4661c26f27f8e
Amal PB <321amalpb.com@gmail.com>
2026-07-21 01:50:12 +0530

    postorder traversal

---
+++ tree/easy/postorder_traversal.rb
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Integer[]}
+def postorder_traversal(root)
+        res=[]
+    def postorder(root,res)
+    return if root.nil?
+        postorder(root.left,res)
+        postorder(root.right,res)
+        res<<root.val
+    end
+    postorder(root,res)
+    res
+end
