4f3fe429672b368a0966d707cd44fda387ec36d7
Amal PB <321amalpb.com@gmail.com>
2026-07-21 01:48:11 +0530

    preorder traversal

---
+++ tree/easy/preorder_traversal.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Integer[]}
+def preorder_traversal(root)
+  res = []
+  def preorder(root, res)
+    return if root.nil?
+
+    res << root.val
+
+    preorder(root.left, res)
+    preorder(root.right, res)
+  end
+  preorder(root, res)
+  res
+end
