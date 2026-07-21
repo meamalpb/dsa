a4ed7821ce361c765d1793e7126a2cb29bbbfb67
Amal PB <321amalpb.com@gmail.com>
2026-07-21 14:51:35 +0530

    search in 2d matrix

---
+++ binary_search/medium/search_in_2d_matrix.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+# binary_search/medium/search_in_2d_matrix.rb
+
+# Problem
+# https://leetcode.com/problems/search-a-2d-matrix/
+
+# @param {Integer[][]} matrix
+# @param {Integer} target
+# @return {Boolean}
+def search_matrix(matrix, target)
+    x=matrix[0].length
+    y=matrix.length
+    l=0
+    r=y-1
+    t=nil
+    while l<=r do 
+        m = (l+r)/2
+        if matrix[m][0]==target
+            return true
+        elsif matrix[m][0]>target
+            r=m-1
+        elsif matrix[m][0]<target
+            if matrix[m][x-1]>=target
+                t=m
+                break
+            else
+                l=m+1
+            end
+        end
+    end
+    return false if t.nil?
+
+    left = 0
+    right = x-1
+
+    while left<=right do 
+        mid = (left+right)/2
+        if matrix[t][mid]==target
+            return true
+        elsif matrix[t][mid]>target
+            right=mid-1
+        else
+            left=mid+1
+        end
+    end
+    return false
+end
+
+if __FILE__ == $PROGRAM_NAME
+    p search_matrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]],3)
+    p search_matrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]],13)
+end
